e021b3b37499af5955206ca6f543c9b4c6c76c35
DhanushAdithiya <k.b.dhanush02@gmail.com>
2025-01-22 16:34:10 +0530

    fixed baseUrl

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ import Footer from "./components/Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/nav" element={<Navigation />} />
         <Route path="/" element={<App />} />
